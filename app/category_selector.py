c430ec96ac95d1205e281a3375f8f027aae1c61f
ProjectRepos <chrisfuller@Chriss-MBP.home>
2016-12-27 10:02:51 +0000

    Updated likelyhood

--- app/category_selector.py
+++ app/category_selector.py
@@ -1,9 +1,16 @@
 from collections import Counter
 
+THRESHOLD=1.0
+
 class Category_Selector(object):
     """docstring for Category_Selector."""
     def __init__(self, dm):
         self.dm = dm
+
+    def _likelyhood_matches(self,x):
+        likelyhoods = [{'category':c, 'likelyhood':x.count(c)/float(len(x))}
+                        for c in set(x)]
+        return likelyhoods
 
     def is_exact_match(self, comment):
         matches = [
@@ -11,32 +18,20 @@ class Category_Selector(object):
             x in self.dm.master.find({"comment":comment})]
 
         if len(matches)==0:
-            return {'category':None, 'likelyhood':0.0}
-
-        elif len(matches)==1:
-            return {'category':matches[0], 'likelyhood':100.0}
-
-        elif len(matches)>=2:
-            data = Counter(matches)
-            top_category = data.most_common(1)[0][0]
-            result = {
-                'category':matches[0],
-                'likelyhood':matches.count(top_category)*100./len(matches)
-                    }
-            return result
-
+            return []
+        elif len(matches)>0:
+            return self._likelyhood_matches(matches)
         else:
             raise Exception('Error with exact match')
 
     def suggest_category(self, transaction):
 
         # Check if already indexed
-        result = self.is_exact_match(transaction['comment'])
+        results = self.is_exact_match(transaction['comment'])
 
-        if result['likelyhood']==100.:
-            transaction.update(result)
+        if True in [x['likelyhood']>=THRESHOLD for x in results]:
+            transaction.update({'suggestions':results})
             return transaction
 
-        else:
-            pass
-            # Compute levenshtein distance and make array of suggestions
+        # Compute levenshtein distance and make array of suggestions
+        
