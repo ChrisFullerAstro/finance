11ec0e43773eb1874a0ca572375d814e6abe9ef3
ProjectRepos <chrisfuller@chriss-mbp.home>
2017-01-04 20:53:46 +0000

    Got mongo working

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,4 @@ FROM python:3-onbuild
 COPY . /finance
 WORKDIR /finance
 RUN pip install -r requirements.txt
-ENTRYPOINT ["python"]
-CMD ["app/app.py"]
+CMD python app/app.py
--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-
+import os
 # Flask Config
 UPLOAD_FOLDER = '/Users/chrisfuller/Dropbox/Programs/finance_v2/finance/data/uploads/'
 WTF_CSRF_ENABLED = True
@@ -7,9 +7,11 @@ DEBUG = True
 
 
 #Mongo Finance Config
-MONGO_PORT = 5001
+MONGO_PORT = 27017
 MONGO_DBNAME = 'finance'
+MONGO_HOST = os.environ['DB_PORT_27017_TCP_ADDR']
 
 #Mongo Config
-MONGO2_PORT = 5001
+MONGO2_PORT = 27017
 MONGO2_DBNAME = 'config'
+MONGO_HOST = os.environ['DB_PORT_27017_TCP_ADDR']
