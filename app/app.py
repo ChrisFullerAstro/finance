68e7abc4d12979c8a306a2c29a824d59fa846ff0
Chris Fuller <chrisfuller@chriss-mbp.home>
2017-01-28 14:17:50 +0000

    added some logging needs more

--- app/app.py
+++ app/app.py
@@ -8,6 +8,7 @@ from models import category_selector, loaders
 from forms import forms
 import logging
 import datetime
+import json
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s')
 
@@ -126,48 +127,69 @@ def processtransactions(filename):
 def classfication():
     form = forms.ClassficationForm()
     if request.method == 'POST':
+        logging.info("Post method found")
 
         #check if any cat was SelectField
         if form.ctype.data:
+            logging.info("Examining form result from form category selected={0}".format(form.ctype.data))
             ct = session.get('current_transaction')
             ct.update({'category':form.ctype.data})
             db_finance.db.master.insert_one(loaders.filter_for_master(ct))
+
             if session.get('current_transactions'):
+                logging.info('found current transactions so appending current transaction, current_transactions now {0}'.format(len(session.get('current_transactions'))))
                 session['current_transactions'].append(ct)
             else:
+                logging.info('No current transactions found so creating current transactions in session')
                 session['current_transactions'] = [ct]
 
     #get config
+    logging.info('Getting config')
     cs_config = session.get('config_data', category_selector.get_config(db_config.db.cs_config))
 
     # Get top transaction from the input_data
     current_transactions = session.get('input_data')
-
+    logging.info('Getting input data, len(current_transactions)={0}'.format(len(current_transactions)))
     #if there are no more transactions left redirect
     if not current_transactions or current_transactions==[]:
+        logging.info('no current_transactions found finishing classfication')
         flash('Finished', 'success')
         return redirect(url_for('home'))
 
     session['current_transaction'] = session['input_data'].pop(0)
+    logging.info('current_transaction:{0}'.json.dumps(session['current_transaction'] ))
 
     #suggest_category
     current_transaction, automatic = category_selector.suggest_category(session['current_transaction'], cs_config, db_finance.db.master)
 
+    logging.info('current_transaction after classfication:{0}'.json.dumps(current_transaction))
+
     #test if suggestions in above automatic limit
     if automatic:
+        logging.info('automatic classfication found')
         flash('transaction classfied automatically', 'info')
         ct = session.get('current_transaction')
         ct.update({'category':current_transaction['suggestions'][0]})
-        db_finance.db.master.insert_one(loaders.filter_for_master(ct))
+        logging.info('filter_for_master')
+        fct = loaders.filter_for_master(ct)
+
+        logging.info('current_transaction after auto ready for injection into master:{0}'.json.dumps(fct))
+        db_finance.db.master.insert_one(fct)
+
+        logging.info('current_transaction after auto ready for injection into current_transactions:{0}'.json.dumps(ct))
         if session.get('current_transactions'):
+            logging.info('Adding to current_transactions len(session["current_transactions"])')
             session['current_transactions'].append(ct)
         else:
+            logging.info('Creating current_transactions')
             session['current_transactions'] = [ct]
 
+        logging.info('redirect after automatic classfication')
         return redirect(url_for('classfication'))
 
     #requires user input
     else:
+        logging.info('requires user input')
         form.ctype.choices=[]
         if current_transaction['suggestions'][0]:
             form.ctype.choices.append((current_transaction['suggestions'][0], current_transaction['suggestions'][0]))
