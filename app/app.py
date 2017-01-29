38b6d4f7dcd6616dcc31e024bd17c81b200e349c
Chris Fuller <chrisfuller@chriss-mbp.home>
2017-01-29 21:55:30 +0000

    blind git

--- app/app.py
+++ app/app.py
@@ -87,9 +87,8 @@ def current_transactions():
             return redirect(url_for('current_transactions'))
 
 
-    data = [x for x in db_finance.db.current_transactions.find({})]
-    if data!=[] or data != None:
-        return render_template('render_data.html', data=data, page_header='Current Transactions')
+    if ldb_finance.db.current_transactions.find_one({})
+        return render_template('render_data.html', data=[x for x in db_finance.db.current_transactions.find({})], page_header='Current Transactions')
     else:
         flash('You have no current stored transactions', 'danger')
         return redirect(url_for('home'))
